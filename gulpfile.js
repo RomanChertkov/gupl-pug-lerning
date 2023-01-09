66fd1c59a10fd807852472a11846e47180e20e98
Roman Chertkov <r.chertkov@mail.ru>
2023-01-09 16:50:04 +0700

    fix all

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,11 @@ import buildStyles from './gulp/tasks/buildStyles.js'
 import cleanBuild from './gulp/tasks/cleanBuild.js'
 
 const mainTasks = gulp.series(cleanBuild, gulp.parallel(buildHtml, buildStyles))
+const build =  gulp.series(cleanBuild, buildHtml, buildStyles)
 
 
-export default function(){
+export { build }
+export default  function(){
     gulp.watch('./src/**/*.pug', mainTasks)
     gulp.watch('./src/**/*.scss', mainTasks)
 }
-
-gulp.task('build', async function(){
-   await  mainTasks
-
-})

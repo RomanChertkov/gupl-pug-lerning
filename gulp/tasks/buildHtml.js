380678524bc430f62a9a078222a89cf526c1e01d
Roman Chertkov <r.chertkov@mail.ru>
2023-01-09 18:51:01 +0700

    Update 2023-01-09T11:50:59.272Z

--- gulp/tasks/buildHtml.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp'
-import pug from 'gulp-pug'
-
-export default function buildHtml(){
-    return gulp.src('./src/pug/pages/*.pug')
-        .pipe(pug({pretty:true, verbose:true}))
-        .pipe(gulp.dest("./build"))
-}
--- gulp/tasks/cleanBuild.js
+++
@@ -1,5 +0,0 @@
-import * as clean from 'del'
-
-export default  async function cleanBuild() {
-    return await clean.deleteAsync("./build/*")
-}
